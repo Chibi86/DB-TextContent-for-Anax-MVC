434ffa904c641f966ef703d721e944570c48a1a0
Rasmus Berg <chibi86@hotmail.com>
2016-11-20 13:07:31 +0100

    1.0.0 Small correction
    Small correction in menu.tpl.php, wrong link to setup

--- app/view/text-content/menu.tpl.php
+++ app/view/text-content/menu.tpl.php
@@ -1,4 +1,4 @@
-<a href="setup/">Setup or Restore content database tables</a><br /><br />
+<a href="content/setup/">Setup or Restore content database tables</a><br /><br />
 
 <a href="blog/">Blog index</a><br />
 <a href="page/page/example-page/">Example page</a><br /><br />
