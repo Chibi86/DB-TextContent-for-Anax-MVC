91ccca6f599effd080bbcb0a600f0a3a40c984d4
Rasmus Berg <chibi86@hotmail.com>
2017-03-12 10:50:16 +0100

    v1.1.6 Bugfix; test/config.php
    * Bugfix; test/config.php

--- test/config.php
+++ test/config.php
@@ -1,5 +1,13 @@
 <?php
 /**
+ * Get all configuration details to be able to execute the test suite.
+ *
+ */
+
+include(__DIR__ . "/../autoloader.php");
+include(__DIR__ . "/../vendor/autoload.php");
+
+/**
  * Define essential Anax paths, end with /
  *
  */
